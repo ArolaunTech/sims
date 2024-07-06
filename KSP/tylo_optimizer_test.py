6f30c1f9cd10d032cd522127cf5592dd7fa24f27
Proxima <proxima2433@gmail.com>
2024-07-05 23:37:42 -0700

    Fixed initialization of SingleStageCraft class (was simpler initially but broke when trying to instantiate multiple) Simplified engine/fuel data, now uses utils for information Overhauled tylo_optimizer_test to optimize engine layout as well as plot a good landing/ascent profile     Added simulate_landing_with_craft which does an optimal landing and ascent with a given craft and planet (and landing altitude)         In the future this should probably go into an optimizer-utils file or something     Added a function to calculate payload capacity from a craft (subtract dry fuel tanks and engines from total mass) Added to utils:     Table of mass per fuel type     Table of fuel tank mass per fuel type     Table of stats per engine     Removed initialize function since it was pointless

--- KSP/tylo_optimizer_test.py
+++ KSP/tylo_optimizer_test.py
@@ -6,167 +6,219 @@ import math
 planet_name = "tylo"
 altitude = 9100
 
-utils.initialize()
-
 # Calculate initial orbital velocity
-r = utils.bodies[planet_name]["radius"] + altitude
-ag = utils.GRAV_CONSTANT * utils.bodies[planet_name]["mass"] / (r * r)
-
-vx = math.sqrt(ag * r)
-orbital_velocity = vx
 
 mass_in_orbit = 466650
-engines = [
-    (4200, "XenonGas",   2000,  0.486),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (800,  "LiquidFuel", 60000, 1.53),
-    (380,  "LFOx",       375000, 20.126),
-    (380,  "LFOx",       375000, 20.126),
-    (380,  "LFOx",       375000, 20.126),
-    (380,  "LFOx",       375000, 20.126),
-    (380,  "LFOx",       375000, 20.126),
-    (380,  "LFOx",       375000, 20.126),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-    (305,  "LFOx",       180000, 12.036),
-]
+engines = []
 
-fuel_levels = dict()
+engine_count = {
+    "Nerv": 15,
+    "Rapier": 15,
+    "Dawn": 15,
+}
+min_engine_count = {
+    "Nerv": 15,
+    "Rapier": 15,
+}
+
+def simulate_landing_with_craft(craft, planet_name, altitude, logging = False):
+    r = utils.bodies[planet_name]["radius"] + altitude
+    ag = utils.GRAV_CONSTANT * utils.bodies[planet_name]["mass"] / (r * r)
+
+    vx = math.sqrt(ag * r)
+    orbital_velocity = vx
+
+    # Build list of specific impulse
+    list_of_isp = []
+    for engine in craft.engines:
+        isp = utils.engine_info[engine][0]
+        if isp not in list_of_isp:
+            list_of_isp.append(isp)
+    list_of_isp.sort(reverse=True)
+    next_engines_index = 1
+
+    # Landing
+
+    elapsed_time = 0
+    iteration_count = 0
+    tick_time = 1
+    ticks_per_log = 10
+    craft.toggle_engines_by_isp(list_of_isp[0])
+    while (vx > 0):
+        pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
+                                                           craft.wet_mass, vx,
+                                                           altitude, preset=planet_name)
+
+        current_effective_isp = craft.get_average_isp() * math.cos(pitch_angle)
+        # Check if it's more efficient to turn on some engines
+        if next_engines_index < len(list_of_isp):
+            craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
+            test_pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
+                                                                    craft.wet_mass, vx,
+                                                                    altitude, preset=planet_name)
+            test_effective_isp = craft.get_average_isp() * math.cos(test_pitch_angle)
+            if test_effective_isp > current_effective_isp:
+                pitch_angle = test_pitch_angle
+                if (logging):
+                    print(f"Turning on engines with Isp {list_of_isp[next_engines_index]}")
+                next_engines_index += 1
+            
+            else:
+                craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
+    
+        # Update velocity
+        vx -= tick_time * math.cos(pitch_angle) * craft.total_thrust / craft.wet_mass 
+
+        # Simulate burning
+        craft.simulate_burn(tick_time)
+    
+        # Print stuff
+        pitch_in_degrees = int(pitch_angle * 180 / math.pi)
+        if (logging and iteration_count % ticks_per_log == 0):
+            print(f"T={iteration_count * tick_time}: \tPitch: {pitch_in_degrees}\tVelocity: {int(vx)}")
+        iteration_count += 1
+    if (logging):
+        print("Fuel used per type:")
+    total_fuel_mass = 0
+    for fuel_type, fuel_level in craft.fuel_used.items():
+        mass = fuel_level * utils.fuel_masses[fuel_type]
+        if (logging):
+            print(f"{fuel_type}\t{fuel_level}\tMass: {mass}")
+        total_fuel_mass += mass
+    if (logging):
+        print(f"Total fuel mass: {total_fuel_mass}")
+        print("Landed!")
+    iteration_count = 0
+    if (logging):
+        print("Liftoff!")
+    # Ascent
+    next_engines_index -= 1
+    while (vx < orbital_velocity):
+        pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
+                                                           craft.wet_mass, vx,
+                                                           altitude, preset=planet_name)
+        current_effective_isp = craft.get_average_isp() * math.cos(pitch_angle)
+        # Check if it's more efficient to turn off some engines
+        if next_engines_index >= 1:
+            craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
+            test_pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
+                                                                    craft.wet_mass, vx,
+                                                                    altitude, preset=planet_name)
+            test_effective_isp = craft.get_average_isp() * math.cos(test_pitch_angle)
+            if test_effective_isp > current_effective_isp:
+                pitch_angle = test_pitch_angle
+                if (logging):
+                    print(f"Turning off engines with Isp {list_of_isp[next_engines_index]}")
+                next_engines_index -= 1
+            
+            else:
+                craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
+    
+        # Update velocity
+        vx += tick_time * math.cos(pitch_angle) * craft.total_thrust / craft.wet_mass 
+
+        # Simulate burning
+        craft.simulate_burn(tick_time)
+
+        # Print stuff
+        pitch_in_degrees = int(pitch_angle * 180 / math.pi)
+        if (iteration_count % ticks_per_log == 0):
+            if (logging):
+                print(f"T={iteration_count * tick_time}: \tPitch: {pitch_in_degrees}\tVelocity: {int(vx)}")
+        iteration_count += 1
+
+    if (logging):
+        print("Fuel used per type:")
+    total_fuel_mass = 0
+    for fuel_type, fuel_level in craft.fuel_used.items():
+        mass = fuel_level * utils.fuel_masses[fuel_type]
+        if (logging):
+            print(f"{fuel_type}\t{fuel_level}\tMass: {mass}")
+        total_fuel_mass += mass
+    if (logging):
+        print(f"Total fuel mass: {total_fuel_mass}")
+
+
+def payload_capacity(craft):
+    # Mass - total engine mass - total (used) fuel tank mass
+    mass = craft.wet_mass
+    for engine in craft.engines:
+        mass -= utils.engine_info[engine][4]
+    for fuel_type, amount in craft.fuel_used.items():
+        fuel_mass = utils.fuel_masses[fuel_type] * amount
+        fuel_tank_mass = float(fuel_mass) / utils.fuel_tank_info[fuel_type][1]
+        mass -= (fuel_tank_mass)
+    return mass
+
+def build_engine_array_from_count(engine_count):
+    engine_list = []
+    for a, b in engine_count.items():
+        for i in range(b):
+            engine_list.append(a)
+    return engine_list
+
+improved_last_iteration = True
+
+best_engine_count = engine_count.copy()
+
+# Base case
 
 craft = single_stage.SingleStageCraft()
+fuel_levels = dict()
+engines = build_engine_array_from_count(engine_count)
 craft.manual_instantiate(engines, fuel_levels, mass_in_orbit)
+simulate_landing_with_craft(craft, "tylo", altitude)
+max_payload_remaining = payload_capacity(craft)
+print(f"Reference payload capacity (Rapier/Nerv only): {max_payload_remaining}")
 
-craft.toggle_engines_by_isp(4200)
-craft.toggle_engines_by_isp(800)
-list_of_isp = [4200, 800, 380, 305]
-next_engines_index = 2
-
-# Landing.
-elapsed_time = 0
-iteration_count = 0
-tick_time = 0.1
-ticks_per_log = 100
-
-while (vx > 0):
-    pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
-                                                 craft.wet_mass, vx,
-                                                 altitude, preset=planet_name)
-    current_effective_isp = craft.get_average_isp() * math.cos(pitch_angle)
-    # Check if it's more efficient to turn on some engines
-    if next_engines_index < len(list_of_isp):
-        craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
-        test_pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
-                                                          craft.wet_mass, vx,
-                                                          altitude, preset=planet_name)
-        test_effective_isp = craft.get_average_isp() * math.cos(test_pitch_angle)
-        if test_effective_isp > current_effective_isp:
-            pitch_angle = test_pitch_angle
-            print(f"Turning on engines with Isp {list_of_isp[next_engines_index]}")
-            next_engines_index += 1
-            
+while improved_last_iteration:
+    improved_last_iteration = False
+    # Each iteration:
+    # Try adding one engine and removing one engine, and do a simulation each time
+    for engine, engine_info in utils.engine_info.items():
+        # Try adding an engine
+        test_engine_count = best_engine_count.copy()
+        if engine in test_engine_count:
+            test_engine_count[engine] += 1
         else:
-            craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
-    
-    # Update velocity
-    vx -= tick_time * math.cos(pitch_angle) * craft.total_thrust / craft.wet_mass 
-
-    # Simulate burning
-    craft.simulate_burn(tick_time)
-    
-    # Print stuff
-    pitch_in_degrees = int(pitch_angle * 180 / math.pi)
-    if (iteration_count % ticks_per_log == 0):
-        print(f"T={iteration_count * tick_time}: \tPitch: {pitch_in_degrees}\tVelocity: {int(vx)}")
-    iteration_count += 1
-
-print("Fuel used per type:")
-total_fuel_mass = 0
-for fuel_type, fuel_level in craft.fuel_levels.items():
-    mass = -fuel_level * craft.fuel_masses[fuel_type]
-    print(f"{fuel_type}\t{-fuel_level}\tMass: {mass}")
-    total_fuel_mass += mass
-print(f"Total fuel mass: {total_fuel_mass}")
-print("Landed!")
-iteration_count = 0
-print("Liftoff!")
-# Ascent
-next_engines_index -= 1
-while (vx < orbital_velocity):
-    pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
-                                                 craft.wet_mass, vx,
-                                                 altitude, preset=planet_name)
-    current_effective_isp = craft.get_average_isp() * math.cos(pitch_angle)
-    # Check if it's more efficient to turn off some engines
-    if next_engines_index >= 1:
-        craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
-        test_pitch_angle = utils.constant_altitude_thrust_angle(craft.total_thrust,
-                                                          craft.wet_mass, vx,
-                                                          altitude, preset=planet_name)
-        test_effective_isp = craft.get_average_isp() * math.cos(test_pitch_angle)
-        if test_effective_isp > current_effective_isp:
-            pitch_angle = test_pitch_angle
-            print(f"Turning off engines with Isp {list_of_isp[next_engines_index]}")
-            next_engines_index -= 1
-            
+            test_engine_count[engine] = 1
+        fuel_levels = dict()
+        test_engines = build_engine_array_from_count(test_engine_count)
+        test_craft = single_stage.SingleStageCraft()
+        test_craft.manual_instantiate(test_engines, fuel_levels, mass_in_orbit)
+        simulate_landing_with_craft(test_craft, "tylo", altitude, logging = False)
+        test_payload = payload_capacity(test_craft)
+        if test_payload > max_payload_remaining:
+            print(f"Added {engine}")
+            improved_last_iteration = True
+            max_payload_remaining = test_payload
+            print(f"Payload capacity: {test_payload}")
+            best_engine_count = test_engine_count
+        # Removing an engine
+        test_engine_count = best_engine_count.copy()
+        if engine in test_engine_count:
+            if engine in min_engine_count and test_engine_count[engine] == min_engine_count[engine]:
+                # Can't remove this engine as it's needed for something else
+                continue
+            else:
+                if test_engine_count[engine] > 0:
+                    test_engine_count[engine] -= 1
+                fuel_levels = dict()
         else:
-            craft.toggle_engines_by_isp(list_of_isp[next_engines_index])
-    
-    # Update velocity
-    vx += tick_time * math.cos(pitch_angle) * craft.total_thrust / craft.wet_mass 
+            continue
+        test_engines = build_engine_array_from_count(test_engine_count)
+        craft = single_stage.SingleStageCraft()
+        craft.manual_instantiate(test_engines, fuel_levels, mass_in_orbit)
+        simulate_landing_with_craft(craft, "tylo", altitude)
+        test_payload = payload_capacity(craft)
+        if test_payload > max_payload_remaining:
+            print(f"Removed {engine}")
+            improved_last_iteration = True
+            max_payload_remaining = test_payload
+            print(f"Payload capacity: {test_payload}")
+            best_engine_count = test_engine_count
 
-    # Simulate burning
-    craft.simulate_burn(tick_time)
-    
-    # Print stuff
-    pitch_in_degrees = int(pitch_angle * 180 / math.pi)
-    if (iteration_count % ticks_per_log == 0):
-        print(f"T={iteration_count * tick_time}: \tPitch: {pitch_in_degrees}\tVelocity: {int(vx)}")
-    iteration_count += 1
-
-print("Fuel used per type:")
-total_fuel_mass = 0
-for fuel_type, fuel_level in craft.fuel_levels.items():
-    mass = -fuel_level * craft.fuel_masses[fuel_type]
-    print(f"{fuel_type}\t{-fuel_level}\tMass: {mass}")
-    total_fuel_mass += mass
-print(f"Total fuel mass: {total_fuel_mass}")
+print("Local max reached")
+for a, b in best_engine_count.items():
+    if b > 0:
+        print(f"{a}: {b}")
--- KSP/utils.py
+++ KSP/utils.py
@@ -1,6 +1,10 @@
 # Utility file for some functions that I think could be reusable.
 import math
 
+# Useful constants and data structures
+
+GRAV_CONSTANT = 6.67 / 10.0 ** 11
+
 # Map of common aliases
 aliases = dict()
 
@@ -8,29 +12,79 @@ aliases = dict()
 # Current is a dict of mass in kg and radius in m
 bodies = dict()
 
-GRAV_CONSTANT = 6.67 / 10.0 ** 11
+# Map of fuel type name to mass per unit
+fuel_masses = {
+    "LiquidFuel": 5,
+    "LFOx": 5,
+    "Monopropellant": 4,
+    "XenonGas": 0.1,
+    "SolidFuel": 7.5,
+}
 
-def initialize():
-    aliases["kerbol"] = "the sun"
-    aliases["mun"] = "the mun"
+# Map of fuel type to tuple of fuel count and mass ratio of best fuel tank
+# Tank dry mass = fuel wet mass / (mass ratio)
+fuel_tank_info = {
+    "LiquidFuel": (50, 10.0),
+    "LFOx": (40, 8.0),
+    "Monopropellant": (120, 6.0),
+    "XenonGas": (720, 3.1667),
+    "SolidFuel": (0, 2), # idc about solid fuel. Doesn't make sense to use this line anyway
+}
 
-    bodies["the sun"] = {"mass": 1.7565459 * 10 ** 28, "radius": 261600000}
-    bodies["moho"]    = {"mass": 2.5263314 * 10 ** 21, "radius": 250000}
-    bodies["eve"]     = {"mass": 1.2243980 * 10 ** 23, "radius": 700000}
-    bodies["gilly"]   = {"mass": 1.2420363 * 10 ** 17, "radius": 13000}
-    bodies["kerbin"]  = {"mass": 5.2915158 * 10 ** 22, "radius": 600000}
-    bodies["the mun"] = {"mass": 9.7599066 * 10 ** 20, "radius": 200000}
-    bodies["minmus"]  = {"mass": 2.6457580 * 10 ** 19, "radius": 60000}
-    bodies["duna"]    = {"mass": 4.5154270 * 10 ** 21, "radius": 320000}
-    bodies["ike"]     = {"mass": 2.7821615 * 10 ** 20, "radius": 130000}
-    bodies["dres"]    = {"mass": 3.2190937 * 10 ** 20, "radius": 138000}
-    bodies["jool"]    = {"mass": 4.2332127 * 10 ** 24, "radius": 6000000}
-    bodies["laythe"]  = {"mass": 2.9397311 * 10 ** 22, "radius": 500000}
-    bodies["vall"]    = {"mass": 3.1087655 * 10 ** 21, "radius": 300000}
-    bodies["tylo"]    = {"mass": 4.2332127 * 10 ** 22, "radius": 600000}
-    bodies["bop"]     = {"mass": 3.7261090 * 10 ** 19, "radius": 65000}
-    bodies["pol"]     = {"mass": 1.0813507 * 10 ** 19, "radius": 44000}
-    bodies["eeloo"]   = {"mass": 1.1149224 * 10 ** 21, "radius": 210000}
+# Map of engine name to tuple of (vac isp, fueltype, thrust, fuel/sec, mass)
+# Twinboar and SRBS are skipped because the built-in fuel tank requirement is annoying.
+engine_info = {
+    "Dawn":     (4200, "XenonGas",       2000,    0.486,   250),
+    "Nerv":     (800,  "LiquidFuel",     60000,   1.53,    3000),
+    "Wolfhound":(380,  "LFOx",           375000,  20.126,  3300),
+    "Cheetah":  (355,  "LFOx",           125000,  7.181,   1000),
+    "Poodle":   (350,  "LFOx",           250000,  14.568,  1750),
+    "Terrier":  (345,  "LFOx",           60000,   3.547,   500),
+    "Dart":     (340,  "LFOx",           180000,  10.797,  1000),
+    "Rhino":    (340,  "LFOx",           2000000, 119.968, 9000),
+    "Skiff":    (330,  "LFOx",           300000,  18.54,   1600),
+    "Spark":    (320,  "LFOx",           20000,   1.275,   130),
+    "Swivel":   (320,  "LFOx",           215000,  13.703,  1500),
+    "Skipper":  (320,  "LFOx",           650000,  41.426,  3000),
+    "Ant":      (315,  "LFOx",           2000,    0.129,   20),
+    "Vector":   (315,  "LFOx",           1000000, 64.745,  4000),
+    "Mammoth":  (315,  "LFOx",           4000000, 258.978, 15000),
+    "Reliant":  (310,  "LFOx",           240000,  15.789,  1250),
+    "Mainsail": (310,  "LFOx",           1500000, 98.683,  6000),
+    "Cub":      (310,  "LFOx",           32000,   2.105,   180),
+    "Bobcat":   (310,  "LFOx",           400000,  26.316,  2000),
+    "Mastodon": (305,  "LFOx",           1350000, 90.271,  5000),
+    "Thud":     (305,  "LFOx",           120000,  8.024,   900),
+    "Rapier":   (305,  "LFOx",           180000,  12.036,  2000),
+    "Kodiak":   (300,  "LFOx",           260000,  17.675,  1250),
+    "Spider":   (290,  "LFOx",           2000,    0.141,   20),
+    "Twitch":   (290,  "LFOx",           16000,   1.125,   80),
+    "Puff":     (250,  "Monopropellant", 20000,   2.039,   90),
+}
+
+aliases["kerbol"] = "the sun"
+aliases["mun"] = "the mun"
+
+# Initialize dictionaries. Maybe later we can make this read from a text file instead of it being hardcoded.
+bodies["the sun"] = {"mass": 1.7565459 * 10 ** 28, "radius": 261600000}
+bodies["moho"]    = {"mass": 2.5263314 * 10 ** 21, "radius": 250000}
+bodies["eve"]     = {"mass": 1.2243980 * 10 ** 23, "radius": 700000}
+bodies["gilly"]   = {"mass": 1.2420363 * 10 ** 17, "radius": 13000}
+bodies["kerbin"]  = {"mass": 5.2915158 * 10 ** 22, "radius": 600000}
+bodies["the mun"] = {"mass": 9.7599066 * 10 ** 20, "radius": 200000}
+bodies["minmus"]  = {"mass": 2.6457580 * 10 ** 19, "radius": 60000}
+bodies["duna"]    = {"mass": 4.5154270 * 10 ** 21, "radius": 320000}
+bodies["ike"]     = {"mass": 2.7821615 * 10 ** 20, "radius": 130000}
+bodies["dres"]    = {"mass": 3.2190937 * 10 ** 20, "radius": 138000}
+bodies["jool"]    = {"mass": 4.2332127 * 10 ** 24, "radius": 6000000}
+bodies["laythe"]  = {"mass": 2.9397311 * 10 ** 22, "radius": 500000}
+bodies["vall"]    = {"mass": 3.1087655 * 10 ** 21, "radius": 300000}
+bodies["tylo"]    = {"mass": 4.2332127 * 10 ** 22, "radius": 600000}
+bodies["bop"]     = {"mass": 3.7261090 * 10 ** 19, "radius": 65000}
+bodies["pol"]     = {"mass": 1.0813507 * 10 ** 19, "radius": 44000}
+bodies["eeloo"]   = {"mass": 1.1149224 * 10 ** 21, "radius": 210000}
+
+
 
 # Returns gravitational acceleration minus centripetal acceleration. Positive = downward
 # Takes in parameters of the parent body, altimeter height, and horizontal velocity.
@@ -63,11 +117,10 @@ def effective_gravity(vx, altitude, preset = "", mass = 0, radius = 0):
 def constant_altitude_thrust_angle(thrust, craft_mass, vx, altitude, preset = "", mass = 0, radius = 0):
     accel = float(thrust) / craft_mass
     vertical_accel = effective_gravity(vx, altitude, preset, mass, radius)
-
+    
     # vertical_accel should be accel * sin(angle)
     a = vertical_accel / accel
     if (a > 1):
         # Cannot maintain velocity!
         return math.pi / 2
-
     return math.asin(a)
